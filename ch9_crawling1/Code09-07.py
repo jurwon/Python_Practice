c293c51ab57922c308ebb71fdbfaf0a19310cd87
jurwon <susorrow@naver.com>
2023-12-08 10:08:54 +0900

    crawling 사전 작업

---
+++ ch9_crawling1/Code09-07.py
@@ -0,0 +1,11 @@
+import bs4
+
+webPage = open('C:/PythonTest/Python_note/ch9_crawling1/Sample03.html',
+               'rt', encoding='utf-8').read()
+bsObject = bs4.BeautifulSoup(webPage, 'html.parser')
+
+ul_value = bsObject.find('ul', {'class': 'myClass2'})
+print(ul_value)
+
+li_list = bsObject.findAll('li', {'class': 'myClass3'})
+print(li_list)
